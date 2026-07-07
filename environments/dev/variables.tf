3ed399db3f1036712ecb8cdd190ab4a9e0bb20d1
Lucian S. <stoicam.lucian@gmail.com>
2026-07-07 20:26:01 +0300

    feat: define development environment variables

---
+++ environments/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be deployed."
+  type        = string
+}
