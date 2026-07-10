cf4a31ca00fa943daa3fda4ce3f4364abc3ba5f2
Lucian S. <stoicam.lucian@gmail.com>
2026-07-10 11:15:27 +0300

    feat: add Azure Virtual Network resource

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "core" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "core" {
+name = "vnet-dev-core-weu-001"
+resource_group_name = azurerm_resource_group.core.name
+}
