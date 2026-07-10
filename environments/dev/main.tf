1f505925163cb01a7cf72937af6734decdc1588f
Lucian S. <stoicam.lucian@gmail.com>
2026-07-10 23:11:09 +0300

    feat: add application network security group

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,3 +16,9 @@ resource "azurerm_subnet" "application" {
   virtual_network_name = azurerm_virtual_network.core.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "application" {
+  name                = "nsg-app-dev-weu-001"
+  location            = azurerm_resource_group.core.location
+  resource_group_name = azurerm_resource_group.core.name
+}
