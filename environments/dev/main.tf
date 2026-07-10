9b585667abaa1c1f88036136756d42e07242da6f
Lucian S. <stoicam.lucian@gmail.com>
2026-07-10 22:56:03 +0300

    feat: add application subnet

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -9,3 +9,10 @@ resource "azurerm_virtual_network" "core" {
   resource_group_name = azurerm_resource_group.core.name
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "application" {
+  name                 = "snet-app-dev-weu-001"
+  resource_group_name  = azurerm_resource_group.core.name
+  virtual_network_name = azurerm_virtual_network.core.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
