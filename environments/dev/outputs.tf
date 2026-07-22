e5e7c79392092f311531baf2450e6d1f1a3ac69c
Lucian Stoica <stoicam.lucian@gmail.com>
2026-07-22 08:19:23 +0300

    feat(hosting): add Azure App Service platform capability

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -52,3 +52,43 @@ output "service_health_alert_id" {
   description = "ID of the Azure Service Health activity log alert."
   value       = azurerm_monitor_activity_log_alert.service_health.id
 }
+
+output "app_service_plan_name" {
+  description = "Name of the Linux App Service plan."
+  value       = azurerm_service_plan.application.name
+}
+
+output "app_service_plan_id" {
+  description = "ID of the Linux App Service plan."
+  value       = azurerm_service_plan.application.id
+}
+
+output "linux_web_app_name" {
+  description = "Name of the Linux Web App."
+  value       = azurerm_linux_web_app.application.name
+}
+
+output "linux_web_app_id" {
+  description = "ID of the Linux Web App."
+  value       = azurerm_linux_web_app.application.id
+}
+
+output "linux_web_app_default_hostname" {
+  description = "Default hostname of the Linux Web App."
+  value       = azurerm_linux_web_app.application.default_hostname
+}
+
+output "linux_web_app_https_url" {
+  description = "HTTPS URL of the Linux Web App."
+  value       = "https://${azurerm_linux_web_app.application.default_hostname}"
+}
+
+output "app_service_integration_subnet_id" {
+  description = "ID of the App Service VNet integration subnet."
+  value       = azurerm_subnet.appservice_integration.id
+}
+
+output "linux_web_app_user_assigned_identity_id" {
+  description = "Attached User Assigned Managed Identity resource ID for the Linux Web App."
+  value       = azurerm_user_assigned_identity.application.id
+}
